26b28158c4189374c48615fc1d119e3bc375d3a5
cuijiayong <cuijiayong@sythealth.com>
2019-11-01 11:06:30 +0800

    my first commit

---
+++ src/test/java/com/selfimpr/UtilsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfimpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
