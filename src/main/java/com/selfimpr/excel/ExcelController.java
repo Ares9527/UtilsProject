90452792f8ca88fab09129cdf2eeafb2dcbfd00a
cuijiayong <cuijiayong@sythealth.com>
2019-11-13 14:41:11 +0800

    [+]Excel导入工具初次提交；热部署配置

--- src/main/java/com/selfimpr/excel/ExcelController.java
+++ src/main/java/com/selfimpr/excel/ExcelController.java
@@ -4,13 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Excel工具类接口
+ */
 @RestController
 @RequestMapping("/excel")
 public class ExcelController {
@@ -27,16 +29,42 @@ public class ExcelController {
         }
     }
 
+    /**
+     * 【下载】通过浏览器下载excel
+     *
+     * @return
+     */
     @GetMapping(value = "/browserDownload")
     public ResponseEntity<Resource> browserDownload() {
         log.debug("进入到browserDownload");
         return ExcelUtils.browserDownload(peopleList, People.class, 10, "yue.xlsx");
     }
 
+    /**
+     * 【下载】excel下载到本地
+     */
     @GetMapping(value = "/localDownload")
     public void localDownload() {
         log.debug("进入到localDownload");
         ExcelUtils.localDownload(peopleList, People.class, 10, "E:\\test\\", "yue.xlsx");
+    }
+
+    /**
+     * 【上传】根据路径上传excel
+     */
+    @PostMapping(value = "/uploadExcelByPath")
+    public List<People> uploadExcelByPath() {
+        log.debug("uploadExcelByPath");
+        return ExcelUtils.uploadExcel("E:\\test\\yue.xlsx", People.class);
+    }
+
+    /**
+     * 【上传】通过MultipartFile上传excel
+     */
+    @PostMapping(value = "/uploadExcelByMultipartFile")
+    public List<People> uploadExcelByMultipartFile(@RequestParam(value = "uploadFile") MultipartFile file) {
+        log.debug("uploadExcelByMultipartFile");
+        return ExcelUtils.uploadExcel(file, People.class);
     }
 
 }
--- src/main/java/com/selfimpr/excel/ExcelUtils.java
+++ src/main/java/com/selfimpr/excel/ExcelUtils.java
@@ -1,25 +1,274 @@
 package com.selfimpr.excel;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.BooleanUtils;
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ExcelUtils {
+
+    private final static Logger log = LoggerFactory.getLogger(ExcelUtils.class);
+
+    private final static String EXCEL2003 = "xls";
+
+    private final static String EXCEL2007 = "xlsx";
+
+    // ###################################### Excel 导入 开始 ######################################
+
+    /**
+     * 根据路径导入excel
+     *
+     * @param path
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> uploadExcel(String path, Class<T> cls) {
+        return readExcel(path, null, cls);
+    }
+
+    /**
+     * 根据上传的excel文件导入excel
+     *
+     * @param file
+     * @param cls
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> uploadExcel(MultipartFile file, Class<T> cls) {
+        return readExcel(null, file, cls);
+    }
+
+    /**
+     * 读取excel文件数据
+     *
+     * @param path
+     * @param cls
+     * @param file
+     * @param <T>
+     * @return
+     */
+    private static <T> List<T> readExcel(String path, MultipartFile file, Class<T> cls) {
+        log.debug("执行导入excel操作");
+        String fileName = null;
+        List<T> dataList = new ArrayList<>();
+        Workbook workbook = null;
+        try {
+            InputStream inputStream;
+            if (StringUtils.isNotEmpty(path)) {
+                File localFile = new File(path);
+                fileName = localFile.getName();
+                inputStream = new FileInputStream(localFile);
+            } else {
+                fileName = file.getOriginalFilename();
+                inputStream = file.getInputStream();
+            }
+            if (!fileName.matches("^.+\\.(?i)(xls)$") && !fileName.matches("^.+\\.(?i)(xlsx)$")) {
+                log.error("上传文件格式不正确");
+            }
+            if (fileName.endsWith(EXCEL2003)) {
+                workbook = new HSSFWorkbook(inputStream);
+            }
+            if (fileName.endsWith(EXCEL2007)) {
+                workbook = new XSSFWorkbook(inputStream);
+            }
+            if (workbook != null) {
+                // 类映射  注解 columnName-->bean columns
+                Map<String, List<Field>> classMap = new HashMap<>();
+                List<Field> fieldList = Stream.of(cls.getDeclaredFields()).collect(Collectors.toList());
+                fieldList.forEach(field -> {
+                    ExcelColumn annotation = field.getAnnotation(ExcelColumn.class);
+                    if (annotation != null) {
+                        String columnName = annotation.columnName();
+                        if (StringUtils.isBlank(columnName)) {
+                            // return起到的作用和continue是相同的 语法
+                            return;
+                        }
+                        if (!classMap.containsKey(columnName)) {
+                            classMap.put(columnName, new ArrayList<>());
+                        }
+                        field.setAccessible(true);
+                        classMap.get(columnName).add(field);
+                    }
+                });
+
+                // 索引-->columns
+                Map<Integer, List<Field>> reflectionMap = new HashMap<>();
+                // 默认读取第一个sheet
+                Sheet sheet = workbook.getSheetAt(0);
+
+                boolean firstRow = true;
+                for (int i = sheet.getFirstRowNum(); i <= sheet.getLastRowNum(); i++) {
+                    Row row = sheet.getRow(i);
+                    // 首行，提取注解
+                    if (firstRow) {
+                        for (int j = row.getFirstCellNum(); j <= row.getLastCellNum(); j++) {
+                            Cell cell = row.getCell(j);
+                            String cellValue = getCellValue(cell);
+                            if (classMap.containsKey(cellValue)) {
+                                reflectionMap.put(j, classMap.get(cellValue));
+                            }
+                        }
+                        firstRow = false;
+                    } else {
+                        //忽略空白行
+                        if (row == null) {
+                            continue;
+                        }
+                        try {
+                            T t = cls.newInstance();
+                            // 判断是否为空白行
+                            boolean allBlank = true;
+                            for (int k = row.getFirstCellNum(); k <= row.getLastCellNum(); k++) {
+                                if (reflectionMap.containsKey(k)) {
+                                    Cell cell = row.getCell(k);
+                                    String cellValue = getCellValue(cell);
+                                    if (StringUtils.isNotBlank(cellValue)) {
+                                        allBlank = false;
+                                    }
+                                    List<Field> fields = reflectionMap.get(k);
+                                    fields.forEach(x -> {
+                                        try {
+                                            handleField(t, cellValue, x);
+                                        } catch (Exception e) {
+                                            log.error(String.format("reflect field:%s value:%s exception!", x.getName(), cellValue), e);
+                                        }
+                                    });
+                                }
+                            }
+                            if (!allBlank) {
+                                dataList.add(t);
+                            } else {
+                                log.warn(String.format("row:%s is blank ignore!", i));
+                            }
+                        } catch (Exception e) {
+                            log.error(String.format("parse row:%s exception!", i), e);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error(String.format("parse excel exception!"), e);
+        } finally {
+            if (workbook != null) {
+                try {
+                    workbook.close();
+                } catch (Exception e) {
+                    log.error(String.format("parse excel exception!"), e);
+                }
+            }
+        }
+        return dataList;
+    }
+
+    /**
+     * cell数据类型chcek
+     *
+     * @param cell
+     * @return
+     */
+    private static String getCellValue(Cell cell) {
+        if (cell == null) {
+            return "";
+        }
+        // 旧版写法：Cell.CELL_TYPE_NUMERIC
+        if (cell.getCellType() == CellType.NUMERIC) {
+            if (DateUtil.isCellDateFormatted(cell)) {
+                return DateUtil.getJavaDate(cell.getNumericCellValue()).toString();
+            } else {
+                return new BigDecimal(cell.getNumericCellValue()).toString();
+            }
+        } else if (cell.getCellType() == CellType.STRING) {
+            return StringUtils.trimToEmpty(cell.getStringCellValue());
+        } else if (cell.getCellType() == CellType.FORMULA) {
+            return StringUtils.trimToEmpty(cell.getCellFormula());
+        } else if (cell.getCellType() == CellType.BLANK) {
+            return "";
+        } else if (cell.getCellType() == CellType.BOOLEAN) {
+            return String.valueOf(cell.getBooleanCellValue());
+        } else if (cell.getCellType() == CellType.ERROR) {
+            return "ERROR";
+        } else {
+            return cell.toString().trim();
+        }
+
+    }
+
+    /**
+     * 将数据映射到实体
+     *
+     * @param t
+     * @param value
+     * @param field
+     * @param <T>
+     * @throws Exception
+     */
+    private static <T> void handleField(T t, String value, Field field) throws Exception {
+        Class<?> type = field.getType();
+        if (type == null || type == void.class || StringUtils.isBlank(value)) {
+            return;
+        }
+        if (type == Object.class) {
+            field.set(t, value);
+            // 数字类型
+        } else if (type.getSuperclass() == null || type.getSuperclass() == Number.class) {
+            if (type == int.class || type == Integer.class) {
+                field.set(t, NumberUtils.toInt(value));
+            } else if (type == long.class || type == Long.class) {
+                field.set(t, NumberUtils.toLong(value));
+            } else if (type == byte.class || type == Byte.class) {
+                field.set(t, NumberUtils.toByte(value));
+            } else if (type == short.class || type == Short.class) {
+                field.set(t, NumberUtils.toShort(value));
+            } else if (type == double.class || type == Double.class) {
+                field.set(t, NumberUtils.toDouble(value));
+            } else if (type == float.class || type == Float.class) {
+                field.set(t, NumberUtils.toFloat(value));
+            } else if (type == char.class || type == Character.class) {
+                field.set(t, CharUtils.toChar(value));
+            } else if (type == boolean.class) {
+                field.set(t, BooleanUtils.toBoolean(value));
+            } else if (type == BigDecimal.class) {
+                field.set(t, new BigDecimal(value));
+            }
+        } else if (type == Boolean.class) {
+            field.set(t, BooleanUtils.toBoolean(value));
+        } else if (type == Date.class) {
+            //
+            field.set(t, value);
+        } else if (type == String.class) {
+            field.set(t, value);
+        } else {
+            Constructor<?> constructor = type.getConstructor(String.class);
+            field.set(t, constructor.newInstance(value));
+        }
+    }
+    // ###################################### Excel 导入 结束 ######################################
+
+
+    // ###################################### Excel 导出 开始 ######################################
 
     /**
      * Workbook构建
@@ -31,6 +280,7 @@ public class ExcelUtils {
      * @throws IOException
      */
     private static <T> Workbook writeExcel(List<T> dataList, Class<T> cls, Integer splitCount) {
+        log.debug("执行导出excel操作");
         if (CollectionUtils.isEmpty(dataList)) {
             throw new RuntimeException("导出excel的数据不能为空");
         }
@@ -195,4 +445,5 @@ public class ExcelUtils {
             throw new RuntimeException("生成excel出现异常");
         }
     }
+    // ###################################### Excel 导出 结束 ######################################
 }
