4510d6079d20e906702ca46cb02a5317362ff7c9
cuijiayong <cuijiayong@sythealth.com>
2019-11-14 17:38:33 +0800

    [+]Map工具类初次提交

---
+++ src/test/java/com/selfimpr/fastjson/FastJsonTest.java
@@ -0,0 +1,123 @@
+package com.selfimpr.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.selfimpr.bean.Dog;
+import com.selfimpr.excel.People;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class FastJsonTest {
+
+    /**
+     * 【Entity or List<Entity> 转为 String】
+     *      JSON.toJSONString
+     *
+     *         System.out.println(JSON.toJSONString(dog));
+     *         System.out.println(JSON.toJSONString(dogList));
+     */
+
+    /**
+     * 【String 转为 Entity or List<Entity>】
+     *     JSON.parseObject
+     *     JSON.parseArray
+     *
+     * Dog dog = JSON.parseObject("{\"age\":6,\"color\":\"black\",\"name\":\"旺财\"}", Dog.class);
+     * List<Dog> dogList = JSON.parseArray("[{\"age\":6,\"color\":\"black\",\"name\":\"旺财\"},{\"age\":6,\"color\":\"white\",\"name\":\"旺财1\"}]", Dog.class);
+     */
+
+    /**
+     * 【entity to jsonObject】
+     *      (JSONObject)JSON.toJSON
+     *
+     *         JSONObject jsonObject = (JSONObject)JSON.toJSON(dog);
+     *         System.out.println(jsonObject.get("name"));
+     */
+
+    /**
+     * 【List<entity> to jsonArray】
+     *
+     *         JSONArray jsonArrays = (JSONArray)JSON.toJSON(dogList);
+     *         System.out.println(jsonArrays);
+     */
+
+    /**
+     * 【jsonObject to entity】
+     *
+     *         JSONObject jsonObject = (JSONObject)JSON.toJSON(dog);
+     *         Dog dog1 = JSON.toJavaObject(jsonObject, Dog.class);
+     */
+
+    /**
+     * 【jsonArry to List<entity>】
+     *
+     *         JSONArray jsonArrays = (JSONArray)JSON.toJSON(dogList);
+     *         System.out.println(jsonArrays);
+     */
+
+    Dog dog;
+
+    List<Dog> dogList = new ArrayList<>();
+
+    @Before
+    public void bbb() {
+        dog = new Dog("旺财", 6, "black");
+        Dog dog1 = new Dog("旺财1", 8, "white");
+        dogList.add(dog);
+        dogList.add(dog1);
+    }
+
+    @Test
+    public void object2String() {
+        System.out.println(JSON.toJSONString(dog));// {"age":6,"color":"black","name":"旺财"}
+        System.out.println(JSON.toJSONString(dogList));// [{"age":6,"color":"black","name":"旺财"},{"age":6,"color":"white","name":"旺财1"}]
+    }
+
+    @Test
+    public void string2Object() {
+        Dog dog = JSON.parseObject("{\"age\":6,\"color\":\"black\",\"name\":\"旺财\"}", Dog.class);
+        System.out.println(dog);
+
+        List<Dog> dogList = JSON.parseArray("[{\"age\":6,\"color\":\"black\",\"name\":\"旺财\"},{\"age\":6,\"color\":\"white\",\"name\":\"旺财1\"}]", Dog.class);
+        System.out.println(dogList);
+        System.out.println(dogList.size());
+    }
+
+    @Test
+    public void javaBeans2JsonObject() {
+        JSONObject jsonObject =(JSONObject)JSON.toJSON(dog);
+        System.out.println(jsonObject);
+        System.out.println(jsonObject.get("name"));
+    }
+
+    @Test
+    public void javaBeansList2JsonArray() {
+        JSONArray jsonArrays = (JSONArray)JSON.toJSON(dogList);
+        System.out.println(jsonArrays);
+    }
+
+    @Test
+    public void jsonObject2JavaBeans() {
+        JSONObject jsonObject =(JSONObject)JSON.toJSON(dog);
+        System.out.println("################");
+        Dog dog1 = JSON.toJavaObject(jsonObject, Dog.class);
+        System.out.println(dog1);
+    }
+
+    @Test
+    public void JSONArray2JavaBeansList() {
+        JSONArray jsonArrays = (JSONArray)JSON.toJSON(dogList);
+        List<People> peopleList = new ArrayList<>();
+        for(int i=0;i<jsonArrays.size();i++){
+            People people = JSON.toJavaObject(jsonArrays.getJSONObject(i), People.class);
+            peopleList.add(people);
+        }
+        System.out.println(peopleList.size());
+    }
+}
---
+++ src/test/java/com/selfimpr/map/MapTest.java
@@ -0,0 +1,72 @@
+package com.selfimpr.map;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.selfimpr.bean.Dog;
+import com.selfimpr.excel.People;
+import org.assertj.core.util.Maps;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class MapTest {
+
+    private static People people = new People("小明", 6);
+    private static People people1 = new People("小明1", 7);
+    private static People people2 = new People("小明2", 8);
+
+    @Test
+    public void string2Map() {
+        System.out.println(MapUtils.string2MapByFastJson("{\"name\":\"小明\",\"age\":\"16\",\"color\":\"yellow\"}"));
+    }
+
+    @Test
+    public void entity2Map2() {
+        Map<String, Object> map = MapUtils.entity2MapByFastJson(people);
+        System.out.println(map.get("name"));
+    }
+
+    @Test
+    public void map2Object() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "旺财");
+        map.put("age", "8");
+        map.put("color", "white");
+        System.out.println(MapUtils.map2EntityByFastJson(map, Dog.class));
+    }
+
+    @Test
+    public void mapList2EntityListByFastJson() {
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "小花1");
+        map.put("age", "8");
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("name", "小花2");
+        map1.put("age", "9");
+        mapList.add(map);
+        mapList.add(map1);
+
+        List<People> peopleList = MapUtils.mapList2EntityListByFastJson(mapList, People.class);
+        System.out.println(peopleList);
+        System.out.println(peopleList.size());
+    }
+
+    @Test
+    public void entityList2MapListByFastJson() {
+        List<People> peopleList = new ArrayList<>();
+        peopleList.add(people);
+        peopleList.add(people1);
+        peopleList.add(people2);
+
+        List<Map<String, People>> mapList = MapUtils.entityList2MapListByFastJson(peopleList);
+        System.out.println(mapList);
+        System.out.println(mapList.size());
+        System.out.println(mapList.get(2).get("name"));
+    }
+}
