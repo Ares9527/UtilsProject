69f232de7eb33dd6f074aab5ea2d57264ca55d47
cuijiayong <cuijiayong@sythealth.com>
2020-01-16 20:30:06 +0800

    [+]Quart定时调度demo

---
+++ src/main/java/com/selfimpr/quartz/demo2/HelloJob2.java
@@ -0,0 +1,22 @@
+package com.selfimpr.quartz.demo2;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class HelloJob2 implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 打印当前的执行时间 例如 2017-11-23 00:00:00
+        Date date = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("现在的时间是：" + sf.format(date));
+        // 具体的业务逻辑
+        System.out.println("开始生成任务报表 或 开始发送邮件");
+    }
+
+}
